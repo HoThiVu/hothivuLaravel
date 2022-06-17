387b281be6e2ae9f25d99b2e86ba0b196da10789
HoThiVu <89339469+HoThiVu@users.noreply.github.com>
2022-06-17 11:44:51 +0700

    file show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,23 +8,28 @@
     <title>ho thi vu</title>
 </head>
 <body>
-
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="/img/{{$car['image'] }}" width="200px" height="300px">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">{{$car['id']}}</h5>
-              <p class="card-text">{{ $car['description']}}</p>
-              {{-- <p class="card-text">{{ $car['description']}}</p>  --}}
-            <p class="card-text"><small class="text-muted"> {{$car['make'] }}</small></p>
-              <p class="card-text"><small class="text-muted">{{$car['model'] }}</small></p>
-              <p class="card-text"><small class="text-muted">{{ $car['produced_on']}}</small></p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <table class="table table-bordered table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            <th scope="col">Handle</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{{$car['id']}}</th>
+            <td> <img src="/img/{{$car['image'] }}" width="200px" height="300px"></td>
+            <td>{{ $car['description']}}</td>
+            <td>{{$car['make'] }}</td>
+            <td>{{$car['model'] }}</td>
+            <td>{{ $car['produced_on']}}</td>
+          </tr>
+         
+        </tbody>
+      </table>
 </body>
 </html>
